28e460532aa3cb4be1bad0132c235a8c95f2a449
Junjie Chen <chris.jjc@outlook.com>
2025-09-12 16:57:10 +0900

    add flag for max model length

--- example_scripts/start_vllm_server.sh
+++ example_scripts/start_vllm_server.sh
@@ -15,13 +15,13 @@ DP_SIZE=${DP_SIZE:-1}
 python -m vllm.entrypoints.openai.api_server \
   --model "$MODEL_ID" \
   --port "$PORT" \
-  --tensor-parallel-size "$TP_SIZE" \
-  --data-parallel-size "$DP_SIZE" \
-  --served-model-name "$MODEL_ID" \
-  --gpu-memory-utilization "$GPU_UTIL" \
+  --tensor-parallel-size $TP_SIZE \
+  --data-parallel-size $DP_SIZE \
+  --served-model-name $MODEL_ID \
+  --gpu-memory-utilization $GPU_UTIL \
   --quantization "bitsandbytes" \
   --max-num-seqs 256 \
   --max-num-batched-tokens 32768 \
   --enable-prefix-caching \
-  --max_model_len 8192
+  --max_model_len $MAX_MODEL_LEN
 
